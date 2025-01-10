a42dc7b17c9823bd4fb9b3a5ebcbb092495ca9c0
0x996007 <xi4oz3ro1@proton.me>
2025-01-10 10:29:35 +0800

    feat: template

---
+++ wagmi.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from '@wagmi/cli'
+import { actions } from '@wagmi/cli/plugins'
+
+export default defineConfig({
+  out: 'src/abis.ts',
+  contracts: [],
+  plugins: [
+    actions(),
+  ],
+})
